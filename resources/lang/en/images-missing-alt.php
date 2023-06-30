a3372e6e4b9242b7d796722c4e341d3737517ec0
Jason Bradberry <jason@futureselves.uk>
2023-06-30 10:36:55 +0100

    update widget text for improved clarity

--- resources/lang/en/images-missing-alt.php
+++ resources/lang/en/images-missing-alt.php
@@ -1,10 +1,10 @@
 <?php
 
 return [
-    'title'               => 'Assets without Alt Text',
-    'title-for-container' => 'Assets without Alt Text in :container',
-    'edit'                => 'Edit this Asset',
+    'title'               => 'Images without Alt Text',
+    'title-for-container' => 'Images without Alt Text in :container',
+    'edit'                => 'Edit this image',
     'explanation'         => 'It\'s important to add alt text describing your images. This helps users who depend on assistive technology.',
-    'count'               => '{0}|{1}You have :amount image that need attention.|[2,*]You have at least :amount images that need attention.',
-    'done'                => 'All assets have an alt text.',
+    'count'               => '{0}|{1}You have :amount image that needs attention.|[2,*]You have at least :amount images that need attention.',
+    'done'                => 'All images have an alt text.',
 ];
