6037340cc570a17a245c1834d469ad911af01b5f
Kyle Werner <kyle.werner@novu.ch>
2023-09-22 09:10:42 +0200

    add a job to process the workload withoutOverlapping and ShouldBeUniqueUntilProcessing

---
+++ src/Jobs/UpdateMissingAltCacheJob.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Teamnovu\StatamicImagesMissingAlt\Jobs;
+
+use Statamic\Assets\Asset;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUniqueUntilProcessing;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\Middleware\WithoutOverlapping;
+use Illuminate\Queue\SerializesModels;
+
+class UpdateMissingAltCacheJob implements ShouldQueue, ShouldBeUniqueUntilProcessing
+{
+    use Dispatchable;
+    use InteractsWithQueue;
+    use Queueable;
+    use SerializesModels;
+
+    public $tries = 2;
+    protected $event;
+
+    /**
+     * @return void
+     */
+    public function __construct($event)
+    {
+        $this->event = $event;
+    }
+
+    public function handle(\Teamnovu\StatamicImagesMissingAlt\Services\Service $service)
+    {
+        /** @var Asset $asset */
+        $asset = $this->event->asset;
+        $container = $asset->container();
+
+        $service->clearCache($container->handle());
+        $service->preloadCache($container->handle());
+    }
+
+    /**
+     * Get the middleware the job should pass through.
+     *
+     * @return array<int, object>
+     */
+    public function middleware()
+    {
+        return [(new WithoutOverlapping())->releaseAfter(60)->expireAfter(180)];
+    }
+}
